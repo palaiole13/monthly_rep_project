e5fb364d60264249654516efa2f8cb69452a9373
Elli Palaiologou <elli.palaiologou@gmail.com>
2024-03-27 18:43:47 +0000

    register function

--- monthly_rep/views.py
+++ monthly_rep/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import MonthlyReport
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 @login_required
 def view_reports(request):
     reports = MonthlyReport.objects.filter(user=request.user)
-    return render(request, 'monthly_rep/reports.html', {'reports': reports})
+    return render(request, 'reports.html', {'reports': reports})
 
 
 @login_required
@@ -14,8 +15,18 @@ def submit_report(request):
         content = request.POST.get('content')
         MonthlyReport.objects.create(user=request.user, content=content)
         return redirect('view_reports')
-    return render(request, 'monthly_rep/submit_report.html')
+    return render(request, 'submit_report.html')
 
 
 def home(request):
     return render(request, 'monthly_rep/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
